9d0224ad18021054f47968e14fa37249e3a7083c
vanhoan01 <92982928+vanhoan01@users.noreply.github.com>
2022-01-16 09:23:38 +0700

    read_book

--- app/src/main/java/com/example/appbookstore/PageFragment.java
+++ app/src/main/java/com/example/appbookstore/PageFragment.java
@@ -1,5 +1,8 @@
 package com.example.appbookstore;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -9,6 +12,11 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -50,22 +58,68 @@ public class PageFragment extends Fragment {
 
     private TextView txtPage;
     private View mView;
-    private ImageView img;
+    private ImageView img, imgPage;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         mView =  inflater.inflate(R.layout.fragment_page, container, false);
-        txtPage = mView.findViewById(R.id.page);
+        imgPage = mView.findViewById(R.id.imageViewAnh);
         img = mView.findViewById(R.id.image);
 
         Bundle bundleFrm = getArguments();
         if(bundleFrm != null){
             Pages pages = (Pages) bundleFrm.get("data");
             if(pages != null){
-                txtPage.setText(pages.getContent());
+                try {
+                    String url = "http://192.168.1.3/Bookstore_android/public/bookstore/image/" + pages.getContent();
+                    new LoadImageInternet(imgPage).execute(url);
+                }catch (Exception e){
+                    //holder.imgAnh.setImageResource(R.drawable.th1);
+                }
             }
         }
         return mView;
+    }
+    public Bitmap loadImageInternet(String imageUrl) {
+        Bitmap bitmapHinh = null;
+        try {
+            URL url = new URL(imageUrl);
+            InputStream inputStream = url.openConnection().getInputStream();
+            bitmapHinh = BitmapFactory.decodeStream(inputStream);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return bitmapHinh;
+    }
+
+    public class LoadImageInternet extends AsyncTask<String, Void, Bitmap> {
+        ImageView bmImage;
+
+        public LoadImageInternet(ImageView bmImage) {
+            this.bmImage = bmImage;
+        }
+        Bitmap bitmapHinh = null;
+        @Override
+        protected Bitmap doInBackground(String... strings) {
+            try {
+                URL url = new URL(strings[0]);
+                InputStream inputStream = url.openConnection().getInputStream();
+                bitmapHinh = BitmapFactory.decodeStream(inputStream);
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return bitmapHinh;
+        }
+
+        @Override
+        protected void onPostExecute(Bitmap bitmap) {
+            super.onPostExecute(bitmap);
+            bmImage.setImageBitmap(bitmap);
+        }
     }
 }
